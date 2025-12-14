7aca832d36b8bccd94c9a2d74e820b4f7c6af5ac
Ger Hobbelt <ger@hobbelt.com>
2025-12-14 21:20:57 +0100

    corrections to our monolithic build sources, following the latest merges.

--- tests/monolithic-main.c
+++ tests/monolithic-main.c
@@ -8,7 +8,7 @@
 MONOLITHIC_CMD_TABLE_START()
 	{ "agrep", { .fa = tre_agrep_main } },
 	{ "bench", { .fa = tre_bench_test_main } },
-	{ "randtest", { .fa = tre_randtest_main } },
+	{ "randtest", { .f = tre_randtest_main } },
 	{ "retest", { .fa = tre_retest_main } },
 	{ "test_str_source", { .fa = tre_test_str_source_main } },
     { "raw2str", {.fa = tre_rewrite_raw_strings_util_main } },
--- tests/monolithic_examples.h
+++ tests/monolithic_examples.h
@@ -9,7 +9,7 @@ extern "C" {
 
 extern int tre_agrep_main(int argc, const char** argv);
 extern int tre_bench_test_main(int argc, const char** argv);
-extern int tre_randtest_main(int argc, const char** argv);
+extern int tre_randtest_main(void);
 extern int tre_retest_main(int argc, const char** argv);
 extern int tre_test_str_source_main(int argc, const char** argv);
 extern int tre_rewrite_raw_strings_util_main(int argc, const char** argv);
